fe208b2718047dbe8e29368a6d04d2b8c9a6c94b
Taemin Kim <taeminkimny@gmail.com>
2022-05-18 12:03:51 -0400

    commit tree 0d5ae9e18e069f846fabcd322833e7ea2f398233

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+let employees = {
+    name : 'kevin',
+    streetAddress : 'windsor', 
+}
+
+function updateEmployeeWithKeyAndValue(employees, name, val) {
+    let copyOfEmployees = {...employees};
+    copyOfEmployees.name = val
+
+    return copyOfEmployees;
+}
